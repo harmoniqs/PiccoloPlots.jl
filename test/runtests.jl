9fad09d01d67d08ed26e5ab29fc829fefb6cd2cb
Gennadi Ryan <gennadiryan@gmail.com>
2025-07-15 03:49:36 -0400

    Adding imports in correct order to ensure plotting methods are defined ahead of running package tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,7 @@
 using Test
 using TestItemRunner
 # using QuantumToolbox, Makie, GLMakie, Colors, PiccoloPlots
+using QuantumCollocation, QuantumToolbox, PiccoloPlots # PiccoloPlotsQuantumToolboxExt must be loaded (via import of the associated weakdeps) to ensure the methods PiccoloPlots.plot_{bloch,wigner} are defined ahead of running package tests
 
 # Run all testitem tests in package
 @run_package_tests
