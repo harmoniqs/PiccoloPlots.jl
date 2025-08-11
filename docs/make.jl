aad54ee20335a25b1207bfee2f8f6f75cfd804fe
Jack Champagne <jackchampagne.r@gmail.com>
2025-08-11 04:36:50 -0400

    use PiccoloDocsTemplate instead of utils.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,18 +1,11 @@
 using PiccoloPlots
+using PiccoloDocsTemplate
 
 pages = [
     "Home" => "index.md",
     "Quickstart Guide" => "generated/quickstart.md",
     "Library" => "lib.md"
 ]
-
-# Check if utils.jl exists and warn if not found
-utils_path = joinpath(@__DIR__, "utils.jl")
-if !isfile(utils_path)
-    error("docs/utils.jl is required but not found. Please run get_docs_utils.sh")
-end
-
-include("utils.jl")
 
 generate_docs(
     @__DIR__,
